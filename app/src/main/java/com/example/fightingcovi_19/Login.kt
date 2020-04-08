f32218dfb1d55467010f51552308f6ef6a5e575d
COVID-19-dotcom <covid0398@gmail.com>
2020-04-08 20:15:46 +0530

    COVID19 Android App

---
+++ app/src/main/java/com/example/fightingcovi_19/Login.kt
@@ -0,0 +1,80 @@
+package com.example.fightingcovi_19
+
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_regster_user.*
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        floatButtonLogin.setOnClickListener {
+            if (isConnected()) {
+                floatButtonLogin.visibility = View.GONE
+                pbLoginProgress.visibility = View.VISIBLE
+                var flag = false
+                var email = ""
+                var user = ""
+                val firebaseRef = FirebaseDatabase.getInstance().getReference("Login")
+                firebaseRef.addValueEventListener(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Failed to Connect to Server",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+                    override fun onDataChange(p0: DataSnapshot) {
+                        for (snap in p0.children) {
+                            val value = snap.getValue(RegisterUserDataClass::class.java)
+                            if (value!!.email == edtLoginUsername.text.toString() || value!!.password == edtLoginPassword.text.toString()) {
+                                email = value.email
+                                user = value.username
+                                flag = true
+                                break
+                            }
+                        }
+                        if (flag) {
+                            val pref = getSharedPreferences("Login", Context.MODE_PRIVATE)
+                            val edit = pref.edit()
+                            edit.putString("username", user)
+                            edit.putString("email", email)
+                            edit.putString("type", "user")
+                            edit.apply()
+                            startActivity(Intent(this@Login, UserDashboard::class.java))
+                            finish()
+                        } else {
+                            error_msg.error = "Invalid Username or Password!!"
+                            floatButtonLogin.visibility = View.VISIBLE
+                            pbLoginProgress.visibility = View.GONE
+                        }
+                    }
+                })
+            }else{
+                Toast.makeText(applicationContext,"No Internet Connection,Try Again!",Toast.LENGTH_LONG).show()
+            }
+        }
+        txtGotoSignUp.setOnClickListener {
+            startActivity(Intent(this,RegsterUser::class.java))
+        }
+    }
+
+    private fun isConnected() : Boolean{
+        val connMrg = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val netInfo = connMrg.activeNetworkInfo
+        return netInfo != null && netInfo.isConnected
+    }
+}
---
+++ app/src/main/java/com/example/fightingcovi_19/UserResult.kt
@@ -0,0 +1,284 @@
+package com.example.fightingcovi_19
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.telephony.SmsManager
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
+import kotlinx.android.synthetic.main.activity_user_result.*
+
+class UserResult : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_result)
+
+            check()
+        SendSMS.setOnClickListener {
+                sendSMS()
+         }
+        imgbutton.setOnClickListener {
+            val intent=Intent(this,UserDashboard::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun sendSMS()
+    {
+
+        var pref = getSharedPreferences("Login",Context.MODE_PRIVATE)
+        var intentkey1=intent
+        var key2=intentkey1.getStringExtra("key2")
+        var drname=intentkey1.getStringExtra("drname")
+        var username=pref.getString("username",null)
+        var num=key2
+        val messageToSend = "Dr. $drname,\n " +
+                "Your Patient with name $username need some Advice regarding COVID-19." +
+                "Please Make him/her Call for the same."+
+                "Don't Replay here..."
+               // "Stay Home and Stay Safe"
+        val number = num
+        SmsManager.getDefault().sendTextMessage(number,null,messageToSend,null,null)
+        val contextView = findViewById<View>(android.R.id.content)
+        val snack=Snackbar.make(contextView,"Sms Will be Sended.",Snackbar.LENGTH_LONG)
+        snack.show()
+    }
+    private fun check()
+    {
+
+        val intentkey1=intent
+        val key1=intentkey1.getStringExtra("key1")
+        //var key2=intentkey1.getStringExtra("key2")
+        //var num=key2
+        if (key1.toString().toInt() ==75)
+        {
+            val circularProgressBar = findViewById<CircularProgressBar>(R.id.circularProgressBar)
+            circularProgressBar.apply {
+                // Set Progress
+                progress = 100f
+                // or with animation
+                setProgressWithAnimation(83f, 3000) // =1s
+
+                // Set Progress Max
+                progressMax = 100f
+
+                // Set ProgressBar Color
+                progressBarColor = Color.BLUE
+                // or with gradient
+                progressBarColorStart = Color.parseColor("#13547a")
+                progressBarColorEnd = Color.parseColor("#80d0c7")
+                progressBarColorDirection = CircularProgressBar.GradientDirection.RIGHT_TO_LEFT
+
+                // Set background ProgressBar Color
+                backgroundProgressBarColor = Color.GRAY
+                // or with gradient
+                //   backgroundProgressBarColorStart = Color.WHITE
+                //  backgroundProgressBarColorEnd = Color.RED
+                backgroundProgressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+                // Set Width
+                progressBarWidth = 29f // in DP
+                backgroundProgressBarWidth = 20f // in DP
+
+                // Other
+                roundBorder = true
+                startAngle = 180f
+                progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+            }
+            txt_result.text ="Your result is upto 75%...\nYou need Doctor attention."
+        }
+        if (key1.toString().toInt() ==70)
+        {
+            val circularProgressBar = findViewById<CircularProgressBar>(R.id.circularProgressBar)
+            circularProgressBar.apply {
+                // Set Progress
+                progress = 100f
+                // or with animation
+                setProgressWithAnimation(70f, 3000) // =1s
+
+                // Set Progress Max
+                progressMax = 100f
+
+                // Set ProgressBar Color
+                progressBarColor = Color.BLUE
+                // or with gradient
+                progressBarColorStart = Color.parseColor("#13547a")
+                progressBarColorEnd = Color.parseColor("#80d0c7")
+                progressBarColorDirection = CircularProgressBar.GradientDirection.RIGHT_TO_LEFT
+
+                // Set background ProgressBar Color
+                backgroundProgressBarColor = Color.GRAY
+                // or with gradient
+                //   backgroundProgressBarColorStart = Color.WHITE
+                //  backgroundProgressBarColorEnd = Color.RED
+                backgroundProgressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+                // Set Width
+                progressBarWidth = 29f // in DP
+                backgroundProgressBarWidth = 20f // in DP
+
+                // Other
+                roundBorder = true
+                startAngle = 180f
+                progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+            }
+            txt_result.text ="Your result is upto 70%...\nYou need Doctor attention."
+        }
+        if (key1.toString().toInt() ==80)
+        {
+            val circularProgressBar = findViewById<CircularProgressBar>(R.id.circularProgressBar)
+            circularProgressBar.apply {
+                // Set Progress
+                progress = 100f
+                // or with animation
+                setProgressWithAnimation(80f, 3000) // =1s
+
+                // Set Progress Max
+                progressMax = 100f
+
+                // Set ProgressBar Color
+                progressBarColor = Color.BLUE
+                // or with gradient
+                progressBarColorStart = Color.parseColor("#13547a")
+                progressBarColorEnd = Color.parseColor("#80d0c7")
+                progressBarColorDirection = CircularProgressBar.GradientDirection.RIGHT_TO_LEFT
+
+                // Set background ProgressBar Color
+                backgroundProgressBarColor = Color.GRAY
+                // or with gradient
+                //   backgroundProgressBarColorStart = Color.WHITE
+                //  backgroundProgressBarColorEnd = Color.RED
+                backgroundProgressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+                // Set Width
+                progressBarWidth = 29f // in DP
+                backgroundProgressBarWidth = 20f // in DP
+
+                // Other
+                roundBorder = true
+                startAngle = 180f
+                progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+            }
+            txt_result.text ="Your result is upto 80%...\nYou need Doctor attention."
+        }
+        if (key1.toString().toInt() ==55)
+        {
+            val circularProgressBar = findViewById<CircularProgressBar>(R.id.circularProgressBar)
+            circularProgressBar.apply {
+                // Set Progress
+                progress = 100f
+                // or with animation
+                setProgressWithAnimation(55f, 3000) // =1s
+
+                // Set Progress Max
+                progressMax = 100f
+
+                // Set ProgressBar Color
+                progressBarColor = Color.BLUE
+                // or with gradient
+                progressBarColorStart = Color.parseColor("#13547a")
+                progressBarColorEnd = Color.parseColor("#80d0c7")
+                progressBarColorDirection = CircularProgressBar.GradientDirection.RIGHT_TO_LEFT
+
+                // Set background ProgressBar Color
+                backgroundProgressBarColor = Color.GRAY
+                // or with gradient
+                //   backgroundProgressBarColorStart = Color.WHITE
+                //  backgroundProgressBarColorEnd = Color.RED
+                backgroundProgressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+                // Set Width
+                progressBarWidth = 29f // in DP
+                backgroundProgressBarWidth = 20f // in DP
+
+                // Other
+                roundBorder = true
+                startAngle = 180f
+                progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+            }
+            txt_result.text ="Your result is upto 55%...\nYou need Doctor attention."
+        }
+        if (key1.toString().toInt() ==60)
+        {
+            val circularProgressBar = findViewById<CircularProgressBar>(R.id.circularProgressBar)
+            circularProgressBar.apply {
+                // Set Progress
+                progress = 100f
+                // or with animation
+                setProgressWithAnimation(60f, 3000) // =1s
+
+                // Set Progress Max
+                progressMax = 100f
+
+                // Set ProgressBar Color
+                progressBarColor = Color.BLUE
+                // or with gradient
+                progressBarColorStart = Color.parseColor("#13547a")
+                progressBarColorEnd = Color.parseColor("#80d0c7")
+                progressBarColorDirection = CircularProgressBar.GradientDirection.RIGHT_TO_LEFT
+
+                // Set background ProgressBar Color
+                backgroundProgressBarColor = Color.GRAY
+                // or with gradient
+                //   backgroundProgressBarColorStart = Color.WHITE
+                //  backgroundProgressBarColorEnd = Color.RED
+                backgroundProgressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+                // Set Width
+                progressBarWidth = 29f // in DP
+                backgroundProgressBarWidth = 20f // in DP
+
+                // Other
+                roundBorder = true
+                startAngle = 180f
+                progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+            }
+            txt_result.text ="Your result is upto 60%... chance\nYou need Doctor attention."
+        }
+        if (key1.toString().toInt() ==0)
+        {
+            val circularProgressBar = findViewById<CircularProgressBar>(R.id.circularProgressBar)
+            circularProgressBar.apply {
+                // Set Progress
+                progress = 100f
+                // or with animation
+                setProgressWithAnimation(3f, 3000) // =1s
+
+                // Set Progress Max
+                progressMax = 100f
+
+                // Set ProgressBar Color
+                progressBarColor = Color.BLUE
+                // or with gradient
+                progressBarColorStart = Color.parseColor("#13547a")
+                progressBarColorEnd = Color.parseColor("#80d0c7")
+                progressBarColorDirection = CircularProgressBar.GradientDirection.RIGHT_TO_LEFT
+
+                // Set background ProgressBar Color
+                backgroundProgressBarColor = Color.GRAY
+                // or with gradient
+                //   backgroundProgressBarColorStart = Color.WHITE
+                //  backgroundProgressBarColorEnd = Color.RED
+                backgroundProgressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+                // Set Width
+                progressBarWidth = 29f // in DP
+                backgroundProgressBarWidth = 20f // in DP
+
+                // Other
+                roundBorder = true
+                startAngle = 180f
+                progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+            }
+            txt_result.text ="0% result...... please stay home...."
+        }
+
+    }
+}
